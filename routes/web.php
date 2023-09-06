30085cadfd54770f241335ee8bf9c1d5d6f759e1
Agatha Sari <agathawulandari02@gmail.com>
2023-09-06 14:48:56 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -21,27 +21,18 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-// ini  route untuk pemilik
 Route::prefix('pemilik')
 ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/dashboard', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
     });
 
-// ini  route untuk pendaftar
 Route::prefix('pendaftar')
 ->middleware(['auth', 'user'])
     ->group(function () {
         Route::get('/dashboard', [DashboardUserController::class, 'index'])->name('user.dashboard');
     });
 
-// ini  route untuk penghuni
-Route::prefix('penghuni')
-->middleware(['auth', 'penghuni'])
-    ->group(function () {
-        Route::get('/dashboard', [DahsboardPenghuniController::class, 'index'])->name('penghuni.dashboard');
-    });
-    
 Route::prefix('penghuni')
 ->middleware(['auth', 'penghuni'])
     ->group(function () {
