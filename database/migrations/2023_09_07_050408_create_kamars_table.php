ab1112837477db14082154c3039ec6de6496a908
iamalya <alyasama226@gmail.com>
2023-09-07 12:16:20 +0700

    tabel kamar

---
+++ database/migrations/2023_09_07_050408_create_kamars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kamars', function (Blueprint $table) {
+            $table->id();
+            $table->integer('no');
+            $table->string('tipe');
+            $table->string('deskripsi');
+            $table->string('status');
+            $table->string('slug')->unique();
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kamars');
+    }
+};
