3f58dd7fd7ff6630d104d10c6eb849bc26ffcaab
hidayatullas <sity111120022@gmail.com>
2023-09-19 10:25:55 +0700

    edit

--- resources/views/pages/admin/datauser/index.blade.php
+++ resources/views/pages/admin/datauser/index.blade.php
@@ -9,10 +9,13 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title mt-3">Data Pendaftar <a href="{{ url('pemilik/data-user/create') }}" class="float-end btn btn-outline-success btn-sm mb-2">Tambah</a></h3>
+                        <h3 class="card-title mt-3">Data Pendaftar <a href="{{ route('data-user.create') }}" class="float-end btn btn-outline-success btn-sm mb-2">Tambah</a></h3>
                         
                     </div>
                     <div class="card-body">
+                        @if(Session::has('success'))
+                            <p class="text-success">{{ session('success') }}</p>
+                        @endif
                         <div class="table-responsive">
                             <table id="tb_datauser" class="table table-hover scroll-horizontal-vertical w-100">
                                 <thead>
