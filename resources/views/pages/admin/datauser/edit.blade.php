f4a2940a5e9edfef8145bf856054189b9f2296bc
hidayatullas <sity111120022@gmail.com>
2023-10-22 15:18:28 +0700

    ubah susunan datauser

--- resources/views/pages/admin/datauser/edit.blade.php
+++ resources/views/pages/admin/datauser/edit.blade.php
@@ -28,112 +28,98 @@
 					<form action="{{ route('data-user.update', $item->id) }}" method="POST" enctype="multipart/form-data">
 						@csrf
 						@method('PUT')
-						
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Nama Lengkap</b></label>
-									<div class="col-sm-9">
-										<input value="{{ $item->name }}" name="name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" autocomplete="off" required/>
-										@error('name')
-										{{-- untuk info yang salah yang mana --}}
-										<div class="invalid-feedback">
-											{{ $message }}
-										</div>            
-										@enderror
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Nama Lengkap</b></label>
+									<input name="name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Masukkan Nama" autocomplete="off" required value="{{ $item->name }}"/>
+									@error('name')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Email Address</b></label>
-									<div class="col-sm-9">
-										<input name="email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" value="{{ $item->email }}" disabled autocomplete="off" required/>
-										@error('email')
-										{{-- untuk info yang salah yang mana --}}
-										<div class="invalid-feedback">
-											{{ $message }}
-										</div>            
-										@enderror
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Email Address</b></label>
+									<input name="email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Masukkan Email" autocomplete="off" required value="{{ $item->email }}"/>
+									@error('email')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label" ><b>Role</b></label>
-									<div class="col-sm-9">
-										<select class="form-control @error('role') is-invalid @enderror" name="role" id="role" style="height: 45px">
-											<option value="{{ $item->role }}">{{ $item->role }}</option>
-											<option value="Pemilik">Pemilik</option>
-											<option value="Pendaftar">Pendaftar</option>
-											<option value="Penghuni">Penghuni</option>
-										</select>
-										@error('role')
-										{{-- untuk info yang salah yang mana --}}
-										<div class="invalid-feedback">
-											{{ $message }}
-										</div>            
-										@enderror
-									</div>
+								<div class="form-group">
+									<label class="col-form-label" ><b>Role</b></label>
+									<select class="form-control @error('role') is-invalid @enderror" name="role" id="role" style="height: 45px" required>
+										<option value="{{ $item->role }}">{{ $item->role }}</option>
+										<option value="Pemilik">Pemilik</option>
+										<option value="Pendaftar">Pendaftar</option>
+										<option value="Penghuni">Penghuni</option>
+									</select>
+									@error('role')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Status Akun</b></label>
-									<div class="col-sm-9">
-										<select class="form-control @error('status_akun') is-invalid @enderror" name="status_akun" id="status_akun" style="height: 45px">
-											<option value="{{ $item->status_akun }}">{{ $item->status_akun }}</option>
-											<option value="Terverifikasi">Terverifikasi</option>
-											<option value="Belum Verifikasi">Tidak Terverifikasi</option>
-											<option value="Diblokir">Ditolak</option>
-										</select>
-										@error('status_akun')
-										{{-- untuk info yang salah yang mana --}}
-										<div class="invalid-feedback">
-											{{ $message }}
-										</div>            
-										@enderror
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Status Akun</b></label>
+									<select class="form-control @error('status_akun') is-invalid @enderror" name="status_akun" id="status_akun" style="height: 45px" required>
+										<option value="{{ $item->status_akun }}">{{ $item->status_akun }}</option>
+										<option value="Terverifikasi">Terverifikasi</option>
+										<option value="Belum Verifikasi">Tidak Terverifikasi</option>
+										<option value="Diblokir">Ditolak</option>
+									</select>
+									@error('status_akun')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Nomor Handphone</b></label>
-									<div class="col-sm-9">
-										<input name="hp" id="hp" type="text" class="form-control" value="{{ $item->hp }}" autocomplete="off" placeholder="0821xxxxxx73" required/>
-										@error('hp')
-										{{-- untuk info yang salah yang mana --}}
-										<div class="invalid-feedback">
-											{{ $message }}
-										</div>            
-										@enderror
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Nomor Handphone</b></label>
+									<input name="hp" id="hp" type="text" class="form-control @error('hp') is-invalid @enderror" placeholder="0821xxxxxx73" autocomplete="off" required value="{{ $item->hp }}"/>
 								</div>
 							</div>
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Password</b></label>
-									<div class="col-sm-9">
-										<input name="password" id="password" type="password" class="form-control" value="{{ $item->password }}" disabled autocomplete="off"/>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Password</b></label>
+									<input name="password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password Min. 8" required value="{{ $item->password }}" autocomplete="off" disabled/>
+									@error('password')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Alasan Penolakan</b></label>
-									<div class="col-sm-9">
-										{{--  <input type="text-area" class="form-control" placeholder="Masukkan Alasan Penolakan"/>  --}}
-										<td><textarea name="alasan_penolakan" id="alasan_penolakan" class="form-control" autocomplete="off" placeholder="Masukkan Alasan Penolakan">{{ $item->alasan_penolakan }}</textarea><p style="font-size: 10px"><b>Catatan : </b> Max:255 karakter. Diisi jika status akun di tolak</p></td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Alasan Penolakan</b></label>
+									{{--  <input type="text-area" class="form-control" placeholder="Masukkan Alasan Penolakan"/>  --}}
+									<td><textarea name="alasan_penolakan" id="alasan_penolakan" class="form-control" placeholder="Masukkan Alasan Penolakan" autocomplete="off" value="{{ $item->alasan_penolakan }}"></textarea><p style="font-size: 10px"><b>Catatan : </b> Max:255 karakter. Diisi jika status akun di tolak</p></td>
 								</div>
-							</div> 
+							</div>
 						</div>
+
 						<td colspan="2">
 							<input type="submit" class="float-end btn btn-gradient-primary btn-sm">
 						</td>
--- resources/views/pages/admin/datauser/show.blade.php
+++ resources/views/pages/admin/datauser/show.blade.php
@@ -27,75 +27,89 @@
 						@csrf
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Nama Lengkap</b></label>
-									<div class="col-sm-9 mt-3">
-										{{--  <input name="name" id="name" type="text" class="form-control" placeholder="Masukkan Nama"/>  --}}
-										<td> {{ $item->name }} </td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Nama Lengkap</b></label>
+									<input name="name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Masukkan Nama" autocomplete="off" required value="{{ $item->name }}" disabled/>
+									@error('name')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Email Address</b></label>
-									<div class="col-sm-9 mt-3">
-										{{--  <input name="email" id="email" type="email" class="form-control" placeholder="Masukkan Email"/>  --}}
-										<td> {{ $item->email }} </td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Email Address</b></label>
+									<input name="email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Masukkan Email" autocomplete="off" required value="{{ $item->email }}" disabled/>
+									@error('email')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Role</b></label>
-									<div class="col-sm-9 mt-3">
-										{{--  <select class="form-control" name="role" id="role">
-											<option value="">Pilih</option>
-											<option value="Pemilik">Pemilik</option>
-											<option value="Pendaftar">Pendaftar</option>
-											<option value="Penghuni">Penghuni</option>
-										</select>  --}}
-										<td> {{ $item->role }} </td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label" ><b>Role</b></label>
+									<input name="role" id="role" type="role" class="form-control @error('role') is-invalid @enderror" autocomplete="off" required value="{{ $item->role }}" disabled/>
+									@error('role')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Status Akun</b></label>
-									<div class="col-sm-9 mt-3">
-										{{--  <select class="form-control" name="status_akun" id="status_akun">
-											<option value="">Pilih</option>
-											<option value="Terverifikasi">Terverifikasi</option>
-											<option value="Belum Verifikasi">Tidak Terverifikasi</option>
-											<option value="Diblokir">Ditolak</option>
-										</select>  --}}
-										<td> {{ $item->status_akun }} </td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Status Akun</b></label>
+									<input name="status_akun" id="status_akun" type="status_akun" class="form-control @error('status_akun') is-invalid @enderror" autocomplete="off" required value="{{ $item->status_akun }}" disabled/>
+									@error('status_akun')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Nomor Handphone</b></label>
-									<div class="col-sm-9 mt-3">
-										{{--  <input name="hp" id="hp" type="text" class="form-control" placeholder="0821xxxxxx73"/>  --}}
-										<td> {{ $item->hp }} </td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Nomor Handphone</b></label>
+									<input name="hp" id="hp" type="text" class="form-control @error('hp') is-invalid @enderror" placeholder="0821xxxxxx73" autocomplete="off" required value="{{ $item->hp }}" disabled/>
 								</div>
 							</div>
 							<div class="col-md-6">
-								<div class="form-group row">
-									<label class="col-sm-3 col-form-label"><b>Alasan Penolakan</b></label>
-									<div class="col-sm-9 mt-3">
-										{{--  <input type="text-area" class="form-control" placeholder="Masukkan Alasan Penolakan"/>  --}}
-										{{--  <td><textarea name="alasan_penolakan" id="alasan_penolakan" class="form-control" placeholder="Masukkan Alasan Penolakan"></textarea></td>  --}}
-										<td> {{ $item->alasan_penolakan }} </td>
-									</div>
+								<div class="form-group">
+									<label class="col-form-label"><b>Password</b></label>
+									<input name="password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password Min. 8" required value="{{ $item->password }}" autocomplete="off" disabled/>
+									@error('password')
+									{{-- untuk info yang salah yang mana --}}
+									<div class="invalid-feedback">
+									{{ $message }}
+									</div>            
+									@enderror
 								</div>
 							</div>
 						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label class="col-form-label"><b>Alasan Penolakan</b></label>
+									{{--  <input type="text-area" class="form-control" placeholder="Masukkan Alasan Penolakan"/>  --}}
+									<td><textarea name="alasan_penolakan" id="alasan_penolakan" class="form-control" placeholder="Masukkan Alasan Penolakan" autocomplete="off" disabled>{{ $item->alasan_penolakan }}</textarea><p style="font-size: 10px"><b>Catatan : </b> Max:255 karakter. Diisi jika status akun di tolak</p></td>
+								</div>
+							</div>
+						</div>
+
+						<td colspan="2">
+							<input type="submit" class="float-end btn btn-gradient-primary btn-sm">
+						</td>
 					</form>
 				</div>
             </div>
